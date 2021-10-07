8e477a7cf8d3677bd2048bdeccc9b29a5a6ae046
Evgeny Atamanov <evgeny.atamanov@acmecryptocorp.io>
2021-10-07 06:52:29 +0300

    fix logs

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.post('/', (req, res) => {
     '127.0.0.1',
     'localhost'
   ]
-  console.log('REQ', req);
+  console.log('IP', req.ip);
   console.log('BODY', req.body)
 
   const payload = JSON.parse(req.body.payload)
